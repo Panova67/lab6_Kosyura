a0ebc683c6e6b2db3c37f1d862d76f0385a7c766
Nadezhda Kosyura <nadejdakna2002@gmail.com>
2023-05-14 16:43:12 +0300

    commit 2 in master

--- lab5.cpp
+++ lab5.cpp
@@ -7,7 +7,7 @@
 int main()
 {
 	//master1: change 1;
-	//master2
+	//master2: change 2;
 	//master3
 	//master4
 	int i, M, sum;
@@ -39,7 +39,7 @@ int main()
 	}
 	//second_branch 1
 	//second_branch 2
-	//second_branch 3
+	//second_branch 3: change 2 in master; //master and second_branch have the same priority
 	//second_branch 4
 	for (i = 0; i < M; i++) {
 		if (isalnum(text[i + 1][0])) {
